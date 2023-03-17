210e435123446a0d5e29a0dd9bd8ed024596765f
Amin <>
2023-03-17 16:40:11 +0330

    update 1.2

---
+++ Show/starts.py
@@ -0,0 +1,11 @@
+import db
+import asyncio
+
+
+# Handle /start in group (it doesn't do anything just show error to send it in pv again)
+
+async def FIrst_STart(Client,Message,Text):
+    if db.CheckUserID(Message.from_user.id):
+        await Message.reply(f"جناپ [{Message.from_user.first_name}](tg://user?id={Message.from_user.id}) عزیز شما همین الانشم تو ربات ثبت شدین 😱")
+    else:
+        await Message.reply(f"[{Message.from_user.first_name}](tg://user?id={Message.from_user.id}) عزیز برای ثبت اکانتت باید رباتو استارت کنی 😍")
--- db.py
+++ db.py
@@ -11,3 +11,22 @@ db = connection.MySQLConnection(
 )
 
 Cursor = db.cursor() #=========> Thats What We Need In All Of DataBase's Requests
+
+db.close()
+
+
+
+
+
+#====================== Check if User Id is exist in Database=====================
+
+def CheckUserID(userid):
+    db.connect()
+    try:
+        Cursor.execute(f"SELECT id from status WHERE user_id = {userid}")
+    except:
+        db.close()
+        return False
+    else:
+        db.close()
+        return True
