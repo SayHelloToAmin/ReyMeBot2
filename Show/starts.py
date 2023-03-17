0f4288cab03bfd808f484d546578c447a7acffee
Amin <>
2023-03-17 17:11:10 +0330

    Update starts.py

--- Show/starts.py
+++ Show/starts.py
@@ -4,9 +4,15 @@ import asyncio
 
 # Handle /start in group (it doesn't do anything just show error to send it in pv again)
 
-async def FIrst_STart(Client,Message,Text):
+async def first_start(Client,Message,Text):
     if db.CheckUserID(Message.from_user.id):
-        tag = f'[{Message.from_user.first_name}](tg://user?id={Message.from_user.id})'
-        await Message.reply(f"جناپ {tag} عزیز شما همین الانشم تو ربات ثبت شدین 😱")
+        await Message.reply(f"جناپ [{Message.from_user.first_name}](tg://user?id={Message.from_user.id}) عزیز شما همین الانشم تو ربات ثبت شدین 😱")
     else:
-        await Message.reply(f"{tag} 😍عزیز برای ثبت اکانتت باید رباتو استارت کنی ")
+        await Message.reply(f"😍عزیز برای ثبت اکانتت باید رباتو استارت کنی [{Message.from_user.first_name}](tg://user?id={Message.from_user.id})")
+
+
+
+# Handle /start In Bot Pv (Register User Or Not If he is already registered)
+
+async def second_start(Client,Message,Text):
+    if db.CheckUserID(Message.from_user.id):
