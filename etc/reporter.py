51d9874771bb4316406df94132d2fef8b6870843
farhadBM <95242834+farhad-Evil@users.noreply.github.com>
2023-03-20 13:38:11 +0330

    asf

--- etc/reporter.py
+++ etc/reporter.py
@@ -22,12 +22,12 @@ def exceptf(file_name, function_name , userid):
 
 #ino ok koooon
 
-def report(Client , cursor , db):
+async def report(client):
     db.connect()
     cursor.excuse("SELECT max(id) FROM error")
     Cloud = cursor.fetchone()
     if Cloud[0] >= 5:
-        Client.send_message(chat_id=908641353,text=f"{Cloud[0]} error has been recorded.")
-        Client.send_message(chat_id=383456888,text=f"{Cloud[0]} error has been recorded.")
+        await client.send_message(chat_id=908641353,text=f"{Cloud[0]} error has been recorded.")
+        await client.send_message(chat_id=383456888,text=f"{Cloud[0]} error has been recorded.")
     else:
         pass
