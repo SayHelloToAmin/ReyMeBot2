3c802a1a44e04feb930cabd45bdf08a72890dc76
Amin <71891311+SayHelloToAmin@users.noreply.github.com>
2023-04-03 22:01:58 +0330

    Update toppm.py

--- Show/toppm.py
+++ Show/toppm.py
@@ -21,18 +21,16 @@ async def TopPm(Client,Message,text):
         num += 1
         if num == 1:
             maintext += f"""【 ♛ {Cloud[0]} with {Cloud[1]} pm !
-
 """
         elif num == 2:
             maintext += f"""【 ♚ {Cloud[0]} with {Cloud[1]} !
-
 """
         else:
             maintext += f"""【 {num}  -  ♙ {Cloud[0]} with {Cloud[1]}
-
 """
         if num <= 8:
-            maintext += "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
+            maintext += """┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
+            """
             
     await Message.reply(maintext)
     maintext = """⬳ ترتیب کاربران ربات بر اساس تعداد پیام ⤺
