f7d73533c08c1756793f90ef157fdd9b1d908681
Amin <71891311+SayHelloToAmin@users.noreply.github.com>
2023-04-03 19:23:30 +0330

    update

--- Show/muted_and_by.py
+++ Show/muted_and_by.py
@@ -7,14 +7,17 @@ async def ShowMutedBy(Client,Message,text):
         if CheckUserID(Message.reply_to_message.from_user.id):
             userid1 = Message.from_user.id
             userid2 = Message.reply_to_message.from_user.id
-            Cloud = mutedby(userid1,userid2)
-            if Cloud:
-                await Message.reply(f"""| بنظر میرسه [{Message.reply_to_message.from_user.first_name}](tg://user?id={userid2}) تا به حال {Cloud[0]} بار میوتت کرده 🧌
-— و تاریخ آخرین میوتت توسط ایشون ⤺
-⌦ {Cloud[1]}""")
+            if userid1 == userid2:
+                await Message.reply("⬳ فکر نمیکنم تا حالا خودتو میوت کرده باشی!🧌")
             else:
-                await Message.reply("""⬳ این بنده خدا تا حالا میوتت نکرده 🧌
-𝒢𝒪𝒪𝒟 𝐹𝑅𝐼𝐸𝒩𝒟𝒮""")
+                Cloud = mutedby(userid1,userid2)
+                if Cloud:
+                    await Message.reply(f"""| بنظر میرسه [{Message.reply_to_message.from_user.first_name}](tg://user?id={userid2}) تا به حال {Cloud[0]} بار میوتت کرده 🧌
+    — و تاریخ آخرین میوتت توسط ایشون ⤺
+    ⌦ {Cloud[1]}""")
+                else:
+                    await Message.reply("""⬳ این بنده خدا تا حالا میوتت نکرده 🧌
+    𝒢𝒪𝒪𝒟 𝐹𝑅𝐼𝐸𝒩𝒟𝒮""")
             
         else:
             await Message.reply(f"😱|این  {Message.reply_to_message.from_user.first_name} چاقال هنوز ثبت نام نکرده ...")
@@ -27,14 +30,17 @@ async def ShowMuted(Client,Message,text):
         if CheckUserID(Message.reply_to_message.from_user.id):
             userid1 = Message.from_user.id
             userid2 = Message.reply_to_message.from_user.id
-            Cloud = muted(userid1,userid2)
-            if Cloud:
-                await Message.reply(f"""⬳ بنظر میرسه [{Message.reply_to_message.from_user.first_name}](tg://user?id={userid2}) رو تا حالا {Cloud[0]} بار میوت کردی 🧌
-— و تاریخ آخرین میوتش توسط تو ⤺
-⌦ {Cloud[1]}""")
+            if userid1 == userid2:
+                await Message.reply("⬳ فکر نمیکنم تا حالا خودتو میوت کرده باشی!🧌")
             else:
-                await Message.reply("""⬳ تا حالا میوتش نکردی ! 🧌 
-𝒢𝒪𝒪𝒟 𝐹𝑅𝐼𝐸𝒩𝒟𝒮""")
+                Cloud = muted(userid1,userid2)
+                if Cloud:
+                    await Message.reply(f"""⬳ بنظر میرسه [{Message.reply_to_message.from_user.first_name}](tg://user?id={userid2}) رو تا حالا {Cloud[0]} بار میوت کردی 🧌
+    — و تاریخ آخرین میوتش توسط تو ⤺
+    ⌦ {Cloud[1]}""")
+                else:
+                    await Message.reply("""⬳ تا حالا میوتش نکردی ! 🧌 
+    𝒢𝒪𝒪𝒟 𝐹𝑅𝐼𝐸𝒩𝒟𝒮""")
             
         else:
             await Message.reply(f"😱|این  {Message.reply_to_message.from_user.first_name} چاقال هنوز ثبت نام نکرده ...")
